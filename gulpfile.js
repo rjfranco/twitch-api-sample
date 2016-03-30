14d9a374a70a0396847e6b20d6b140d622f37e81
Ramiro Jr. Franco <rjfranco@gmail.com>
2016-03-30 10:01:02 -0700

    Adding sass support

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
 var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
+// Process SCSS
+gulp.task('sass', function() {
+  return sass('app/scss/styles.scss')
+    .pipe(gulp.dest('app/styles'))
+    .pipe(reload({ stream: true }));
+});
+
 // watch files for changes and reload
-gulp.task('default', function() {
+gulp.task('serve', ['sass'], function() {
   browserSync({
     server: {
       baseDir: 'app'
     }
   });
 
-  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: 'app'}, reload);
+  gulp.watch(['app/*.html', 'app/scss/*.scss', 'app/scripts/**/*.js'], ['sass'], {cwd: 'app'}, reload);
 });
