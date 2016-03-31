1ccfe6f23f590fc9d6c9864c501bdaf17b1457bb
Ramiro Jr. Franco <rjfranco@gmail.com>
2016-03-31 02:59:34 -0700

    Comitting github page for project.

--- app/scripts/app.js
+++
@@ -1,4 +0,0 @@
-import request from './modules/request';
-import TwitchSearch from './modules/twitch-search';
-
-window.twitch_search = new TwitchSearch();
--- app/scripts/modules/insert-after.js
+++
@@ -1,3 +0,0 @@
-export default function(element, element_to_insert) {
-  element.parentNode.insertBefore(element_to_insert, element.nextSibling);
-}
